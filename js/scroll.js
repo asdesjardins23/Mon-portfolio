0c77cd126151400553e402deca49e9adbae706bf
asdesjardins23 <38906344+asdesjardins23@users.noreply.github.com>
2019-05-02 11:20:59 -0400

    Competences
    Correction de la section competeces. AJout Animate et ScrollReveal.

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+/**
+ * Created by etu03 on 18-02-14.
+ */
+
+// Changer les valeurs par défault
+window.sr = ScrollReveal({ reset: false });
+
+// Personnaliser l'apparition des groupes
+sr.reveal('#a-propos', { duration: 2000 });
+sr.reveal('.services_area', { duration: 2000 });
+sr.reveal('.service_item', { duration: 2000 }, 100);
+sr.reveal('#portfolio', { duration: 2000 });
+/*sr.reveal('#p4', { duration: 200 });
+sr.reveal('#p5', { duration: 200 });
+sr.reveal('#p6', { duration: 200 });
+sr.reveal('.graphique', { duration: 500 });
+sr.reveal('#p7', { duration: 200 });
+sr.reveal('#p8', { duration: 200 });*/
